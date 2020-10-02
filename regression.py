f707b370739f9727ffebddbeb0a4d055e41f38e6
Jed <jamijares3@gmail.com>
2020-10-02 08:17:00 -0500

    adds MSE function

--- regression.py
+++ regression.py
@@ -16,11 +16,20 @@ def generateLine(thetas, xVals):
         yVals += thetas[i] * xVals ** i
     return yVals
 
-data = np.genfromtxt(r'data\synthetic-3.csv', delimiter=',')
+def meanSquaredError(thetas, data):
+    error = 0.0
+    for point in data:
+        sum = 0.0
+        for i in range(len(theta)):
+            sum += theta[i] * (point[0] ** i)
+        error += ((sum - point[1]) ** 2)
+    return error/len(data)
+
+data = np.genfromtxt(r'data\synthetic-1.csv', delimiter=',')
 
 plt.scatter(data[:,0], data[:,1])
 
-theta = [1.0] * 4
+theta = [1.0] * 3
 alpha = (1.0 * 10**-5)
 for i in range(1000):
     for j in range(len(theta)):
@@ -30,4 +39,5 @@ x = np.linspace(-3,3)
 y = theta[0] + theta[1] * x
 second = generateLine(theta, x)
 plt.plot(x, second, '-r', label='second order approximation')
+print(meanSquaredError(theta, data))
 plt.show()
