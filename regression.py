786dddd52c14c6ed80a04937930e7e773fdd64b1
Jed <jamijares3@gmail.com>
2020-10-05 15:20:51 -0500

    creates polynomial class

--- regression.py
+++ regression.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+random.seed(12) # for repeatability
+
 def loss(data, theta, power):
     error = 0.0
     for point in data:
@@ -46,51 +48,30 @@ def plot(theta, x = np.linspace(-3,3), color = '-r'):
     label='order ' + str(len(theta)) + ' approximation'
     plt.plot(x, y, color, label = label) # , label='order' + order + 'approximation')
 
-# # https://stackoverflow.com/a/45395003
-# def sample_floats(low, high, k=1):
-#     """ Return a k-length list of unique random floats
-#         in the range of low <= x <= high
-#     """
-#     result = []
-#     seen = set()
-#     for i in range(k):
-#         x = random.uniform(low, high)
-#         while x in seen:
-#             x = random.uniform(low, high)
-#         seen.add(x)
-#         result.append(x)
-#     return result
-
 # for fileName in [r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthetic-3.csv']:
-for fileName in [r'data/synthetic-2.csv']:
+for fileName in [r'data/synthetic-3.csv']:
+    print(fileName)
     data = np.genfromtxt(fileName, delimiter=',')
 
     plt.scatter(data[:,0], data[:,1])
     plt.ylim(min(data[:,1])-0.5, max(data[:,1])+0.5)
 
-    for orderColor in [(2, '-r'), (3, '-b'), (5, '-y'), (8, '-g')]:
-    # for orderColor in [(5, '-r')]:
-        theta = [0] * orderColor[0]
-        # theta = sample_floats(-2, 2, k = orderColor[0])
+    class Polynomial:
+        def __init__(self, order, color):
+            self.thetaCount = order
+            self.plotColor = color
+
+    for polynomial in [Polynomial(2, '-r'), Polynomial(3, '-b'), Polynomial(5, '-y'), Polynomial(8, '-g')]:
+        theta = [0] * polynomial.thetaCount
         theta = regression(theta, data)
-        # i = 0
         for _ in range(20000):
         # while(meanSquaredError(theta, data) > 15):
             theta = regression(theta, data)
-            # i += 1
-            # print(i)
-        # theta = [1.0] * order
-        # alpha = (1.0 * 10**-5)
-        # for i in range(1000):
-        #     for j in range(len(theta)):
-        #         theta[j] = theta[j] - (alpha * loss(data, theta, j))
 
         # x = np.linspace(-3,3)
         x = np.linspace(min(data[:,0]-0.5), max(data[:,0])+0.5)
-        plot(theta, x, orderColor[1])
-        # y = generateLine(theta, x)
-        # plt.plot(x, y, '-r') # , label='order' + order + 'approximation')
-        print(theta)
-        # print(meanSquaredError(theta, data))
+        plot(theta, x, polynomial.plotColor)
+        # print(theta)
+        print("MSE for order", polynomial.thetaCount - 1, ":", meanSquaredError(theta, data))
     plt.legend()#(loc='upper left')
     plt.show()
