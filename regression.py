f6c1e1cfbe5603fb5e54bc5e984bccd1f409cbb1
Jed <jamijares3@gmail.com>
2020-10-05 16:45:38 -0500

    attempts to get regularization in

--- regression.py
+++ regression.py
@@ -36,19 +36,23 @@ def meanSquaredError(thetas, data):
         error += ((sum - point[1]) ** 2)
     return error/len(data)
 
-def regression(theta, data):
+def regression(theta, data, lambdaValue = 0):
     alpha = (1.0 * 10**-5)
-    # for _ in range(1000):
     for j in range(len(theta)):
-        theta[j] = theta[j] - (alpha * stochasticLoss(data, theta, j))
+        theta[j] = theta[j] - (alpha * stochasticLoss(data, theta, j)) - alpha * lambdaValue * theta[j]
     return theta
 
 def addSubplot(theta, label, x = np.linspace(-3,3), color = '-r'):
     y = generateLine(theta, x)
     plt.plot(x, y, color, label = label) # , label='order' + order + 'approximation')
 
+class Polynomial:
+    def __init__(self, order, color):
+        self.thetaCount = order + 1
+        self.plotColor = color
+
 plt.figure(figsize=(17,12))
-# plt.suptitle("Plots")
+plt.suptitle("Plots")
 for index, fileName in enumerate([r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthetic-3.csv']):
 # for index, fileName in enumerate([r'data/synthetic-3.csv']):
     subplot = plt.subplot(2, 2, index + 1)
@@ -58,14 +62,8 @@ for index, fileName in enumerate([r'data/synthetic-1.csv', r'data/synthetic-2.cs
     plt.scatter(data[:,0], data[:,1])
     plt.ylim(min(data[:,1])-0.5, max(data[:,1])+0.5)
 
-    class Polynomial:
-        def __init__(self, order, color):
-            self.thetaCount = order + 1
-            self.plotColor = color
-
     for polynomial in [Polynomial(1, '-r'), Polynomial(2, '-b'), Polynomial(4, '-y'), Polynomial(7, '-g')]:
         theta = [0] * polynomial.thetaCount
-        theta = regression(theta, data)
         for _ in range(20000):
         # while(meanSquaredError(theta, data) > 15):
             theta = regression(theta, data)
@@ -80,3 +78,33 @@ for index, fileName in enumerate([r'data/synthetic-1.csv', r'data/synthetic-2.cs
     plt.legend()
 # plt.show()
 plt.savefig(r'media/plots.png', bbox_inches='tight')
+plt.close()
+
+plt.figure(figsize=(17,12))
+plt.suptitle("Regularized Plots")
+for index, fileName in enumerate([r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthetic-3.csv']):
+# for index, fileName in enumerate([r'data/synthetic-3.csv']):
+    subplot = plt.subplot(2, 2, index + 1)
+    subplot.set_title(fileName)
+    data = np.genfromtxt(fileName, delimiter=',')
+
+    plt.scatter(data[:,0], data[:,1])
+    plt.ylim(min(data[:,1])-0.5, max(data[:,1])+0.5)
+
+    for polynomial in [Polynomial(1, '-r'), Polynomial(2, '-b'), Polynomial(4, '-y'), Polynomial(7, '-g')]:
+        theta = [0] * polynomial.thetaCount
+        for _ in range(20000):
+        # while(meanSquaredError(theta, data) > 15):
+            theta = regression(theta, data, 25)
+
+        # x = np.linspace(-3,3)
+        x = np.linspace(min(data[:,0]-0.5), max(data[:,0])+0.5)
+        roundedMSE = "{:.2f}".format(meanSquaredError(theta, data))
+        label='order ' + str(len(theta)) + ' approximation (MSE: ' + roundedMSE + ')'
+        addSubplot(theta, label, x, polynomial.plotColor)
+        # print(theta)
+        # print("MSE for order", polynomial.thetaCount - 1, ":", meanSquaredError(theta, data))
+    plt.legend()
+# plt.show()
+plt.savefig(r'media/regularizedPlots.png', bbox_inches='tight')
+plt.close()
