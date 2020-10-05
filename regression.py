ae152c12bf6ea0d38a7d20c79dffc2ffc4fc35b4
Jed <jamijares3@gmail.com>
2020-10-05 15:07:21 -0500

    works pretty well

--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def loss(data, theta, power):
     error = 0.0
@@ -9,6 +10,14 @@ def loss(data, theta, power):
             sum += theta[i] * (point[0] ** i)
         error += (sum - point[1]) * (point[0] ** power)
     return error/len(data)
+
+def stochasticLoss(data, theta, power):
+    point = random.choice(data)
+    sum = 0.0
+    for i in range(len(theta)):
+        sum += theta[i] * (point[0] ** i)
+    error = (sum - point[1]) * (point[0] ** power)
+    return error
 
 def generateLine(thetas, xVals):
     yVals = 0
@@ -25,26 +34,51 @@ def meanSquaredError(thetas, data):
         error += ((sum - point[1]) ** 2)
     return error/len(data)
 
-def regression(data, order):
-    theta = [1.0] * order
+def regression(theta, data):
     alpha = (1.0 * 10**-5)
-    for i in range(1000):
-        for j, value in enumerate(theta):
-            value = value - (alpha * loss(data, theta, j))
+    # for _ in range(1000):
+    for j in range(len(theta)):
+        theta[j] = theta[j] - (alpha * stochasticLoss(data, theta, j))
     return theta
 
 def plot(theta, x = np.linspace(-3,3), color = '-r'):
     y = generateLine(theta, x)
-    plt.plot(x, y, color) # , label='order' + order + 'approximation')
+    label='order ' + str(len(theta)) + ' approximation'
+    plt.plot(x, y, color, label = label) # , label='order' + order + 'approximation')
 
-for fileName in [r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthetic-3.csv']:
+# # https://stackoverflow.com/a/45395003
+# def sample_floats(low, high, k=1):
+#     """ Return a k-length list of unique random floats
+#         in the range of low <= x <= high
+#     """
+#     result = []
+#     seen = set()
+#     for i in range(k):
+#         x = random.uniform(low, high)
+#         while x in seen:
+#             x = random.uniform(low, high)
+#         seen.add(x)
+#         result.append(x)
+#     return result
+
+# for fileName in [r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthetic-3.csv']:
+for fileName in [r'data/synthetic-2.csv']:
     data = np.genfromtxt(fileName, delimiter=',')
 
     plt.scatter(data[:,0], data[:,1])
     plt.ylim(min(data[:,1])-0.5, max(data[:,1])+0.5)
 
-    for order in [2]: #, 3, 5, 8]:
-        theta = regression(data, order)
+    for orderColor in [(2, '-r'), (3, '-b'), (5, '-y'), (8, '-g')]:
+    # for orderColor in [(5, '-r')]:
+        theta = [0] * orderColor[0]
+        # theta = sample_floats(-2, 2, k = orderColor[0])
+        theta = regression(theta, data)
+        # i = 0
+        for _ in range(20000):
+        # while(meanSquaredError(theta, data) > 15):
+            theta = regression(theta, data)
+            # i += 1
+            # print(i)
         # theta = [1.0] * order
         # alpha = (1.0 * 10**-5)
         # for i in range(1000):
@@ -53,8 +87,10 @@ for fileName in [r'data/synthetic-1.csv', r'data/synthetic-2.csv', r'data/synthe
 
         # x = np.linspace(-3,3)
         x = np.linspace(min(data[:,0]-0.5), max(data[:,0])+0.5)
-        plot(theta, x)
+        plot(theta, x, orderColor[1])
         # y = generateLine(theta, x)
         # plt.plot(x, y, '-r') # , label='order' + order + 'approximation')
-        print(meanSquaredError(theta, data))
+        print(theta)
+        # print(meanSquaredError(theta, data))
+    plt.legend()#(loc='upper left')
     plt.show()
